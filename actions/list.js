c3403b3541f5c52e188ac07b71bccb61a25c4d4a
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2014-12-22 13:46:35 +0600

    Added pagination into list and `limit` config option

--- actions/list.js
+++ actions/list.js
@@ -1,32 +1,67 @@
 'use strict';
 var util = require('../lib/adminUtil');
+var requestProcessor = require('../lib/requestProcessor');
 var path = require('path');
+var async = require('async');
 
 module.exports = function(req, res) {
     var Model = util.findModel(req);
     if (!Model) {
         return res.notFound();
     }
-    //Check page
-    var page = req.param('page') || 0;
-    if (!_.isNumber(page) || !_.isFinite(page)) {
-        page = parseInt(page) || 0;
+    var instanceConfig = util.findConfig(req);
+    if (!instanceConfig.list.limit) {
+        instanceConfig.list.limit = 15; //@todo move 15 to constant
     }
-    Model.find()
-        .paginate({page: page})
-        .exec(function(err, list) {
-            if (err) {
-                sails.log.error('Admin list error: ');
-                sails.log.error(err);
-                return res.serverError();
-            }
-            var instanceName = util.findInstanceName(req);
-            res.view(util.getViewPath('list'), {
-                instanceConfig: util.findConfig(req),
-                list: list,
-                instanceName: instanceName,
-                instancePath: path.join(sails.config.admin.routePrefix, instanceName),
-                fields: util.getFields(req, Model, 'list')
-            });
+    //Limit check
+    if (!_.isNumber(instanceConfig.list.limit)) {
+        sails.log.error('Admin list error: limit option should be number. Reseived: ', instanceConfig.list.limit);
+        instanceConfig.list.limit = 15;
+    }
+    //Check page
+    var page = req.param('page') || 1;
+    if (_.isFinite(page)) {
+        page = parseInt(page) || 1;
+    }
+
+    var total = 0;
+    var records = [];
+    async.parallel([
+        //Fetch total records for page
+        function getTotalRecords(done) {
+            Model.count()
+                .exec(function(err, count) {
+                    if (err) return done(err);
+                    total = count;
+                    done();
+                });
+        },
+        // Loading list of records for page
+        function loadRecords(done) {
+            Model.find()
+                .paginate({page: page, limit: instanceConfig.list.limit || 15})
+                .exec(function(err, list) {
+                    if (err) return done(err);
+                    records = list;
+                    done();
+                });
+        }
+    ], function(err, result) {
+        if (err) {
+            sails.log.error('Admin list error: ');
+            sails.log.error(err);
+            return res.serverError(err);
+        }
+        var instanceName = util.findInstanceName(req);
+        res.view(util.getViewPath('list'), {
+            requestProcessor: requestProcessor,
+            instanceConfig: instanceConfig,
+            total: total,
+            list: records,
+            instanceName: instanceName,
+            instancePath: path.join(sails.config.admin.routePrefix, instanceName),
+            fields: util.getFields(req, Model, 'list')
         });
+    });
+
 };
