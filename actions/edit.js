7ca1dfa6c324c616931c924b08b79056c2916acd
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-05-29 01:39:55 -0700

    fix #17

--- actions/edit.js
+++ actions/edit.js
@@ -44,7 +44,9 @@ module.exports = function(req, res) {
                     }
                     var reqData = request.processRequest(req, fields);
                     _.merge(record, reqData); // merging values from request to record
-                    instance.model.update({id: record.id}, reqData).exec(function(err) {
+                    var params = {};
+                    params[req._sails.config.adminpanel.identifierField] = req.param('id');
+                    instance.model.update(params, reqData).exec(function(err) {
                         if (err) {
                             req._sails.log.error(err);
                             req.flash('adminError', err.details || 'Something went wrong...');
