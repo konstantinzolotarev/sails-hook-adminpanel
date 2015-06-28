8a5a1532395391029a13bbbe8eb04213ecff5df6
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-06-28 22:05:59 +0600

    added list styles partially

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
 var del = require('del');
+var less = require('gulp-less');
+var path = require('path');
+
+
+var ASSETS_DIR = 'assets';
+var BOWER_DIR = ASSETS_DIR + '/bower_components/';
+var VENDOR_DIR = ASSETS_DIR + '/vendor/';
 
 var foldersToCopy = [
     'bootstrap/dist',
@@ -18,18 +25,26 @@ var getPathToFile = function(file) {
 };
 
 gulp.task('clear:assets', function() {
-    del(['assets/vendor/*']);
+    del([VENDOR_DIR + '*']);
 });
 
 gulp.task('copy', function() {
     foldersToCopy.forEach(function(folder) {
-        gulp.src('assets/bower_components/' + folder + '/**/*')
-            .pipe(gulp.dest('assets/vendor/' + folder));
+        gulp.src(BOWER_DIR + folder + '/**/*')
+            .pipe(gulp.dest(VENDOR_DIR + folder));
     });
     filesToCopy.forEach(function(file) {
-        gulp.src('assets/bower_components/' + file)
-            .pipe(gulp.dest('assets/vendor/' + getPathToFile(file)));
+        gulp.src(BOWER_DIR + file)
+            .pipe(gulp.dest(VENDOR_DIR + getPathToFile(file)));
     });
+});
+
+gulp.task('less', function() {
+    return gulp.src(ASSETS_DIR + '/styles/importer.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest(ASSETS_DIR + '/styles/'));
 });
 
 gulp.task('default', ['clear:assets', 'copy']);
