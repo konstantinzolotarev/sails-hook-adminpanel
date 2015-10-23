6e4f08c090eafca9af32e23db80d4d2b413a8814
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-10-24 02:29:27 +0600

    els support for may relations

--- helper/viewsHelper.js
+++ helper/viewsHelper.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var path = require('path');
+var _ = require('lodash');
 
 module.exports = {
 
@@ -16,7 +17,7 @@ module.exports = {
      * @param {string} engine - View engine name. E.g. 'jade', 'ejs'...
      * @returns {string}
      */
-    getPathToEngine: function(engine) {
+    getPathToEngine: function (engine) {
         return path.join(this.BASE_VIEWS_PATH, engine, '/')
     },
 
@@ -35,7 +36,44 @@ module.exports = {
      * @param {IncommingMessage} req
      * @param {string} type Types: adminError|adminSuccess
      */
-    hasFlash: function(req, type) {
+    hasFlash: function (req, type) {
         return (req.session.flash && req.session.flash[type]);
+    },
+
+    /**
+     * Get needed field value from dat provided.
+     *
+     * @param {string} key
+     * @param {object} field
+     * @param {Array} data
+     */
+    getFieldValue: function (key, field, data) {
+        var value = data[key];
+        if (_.isObject(value) && field.config.type == 'association') {
+            value = value[field.config.identifierField];
+        }
+        if (_.isArray(value) && field.config.type == 'association-many') {
+            var result = [];
+            value.forEach(function (val) {
+                result.push(val[field.config.identifierField]);
+            });
+            return result;
+        }
+        return value;
+    },
+
+    /**
+     * Check if given option equals value or is in array
+     *
+     * @param {string|number} option
+     * @param {string|number|Array} value
+     * @returns {boolean}
+     */
+    isOptionSelected: function (option, value) {
+        if (_.isArray(value)) {
+            return _.includes(value, option);
+        } else {
+            return (option == value);
+        }
     }
 };
