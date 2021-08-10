2809c15f735e2b3ae074437d66aafa1870ea9262
Siziy Ivan <cziy@ya.ru>
2021-08-10 16:05:28 +0300

    add associations support to datatable

--- actions/list_json.js
+++ actions/list_json.js
@@ -13,23 +13,61 @@ module.exports = async function (req, res) {
         return res.redirect("/admin/userap/login");
 
     let records = [];
+    var fields = fieldsHelper.getFields(req, instance, 'list');
 
-    records = await instance.model.find();
+    let query = instance.model.find();
 
-    let fields = fieldsHelper.getFields(req, instance, "list");
+    fieldsHelper.getFieldsToPopulate(fields).forEach(function(val) {
+        query.populate(val);
+    });
+
+    records = await waterlineExec(query);
+
     let identifierField = configHelper.getIdentifierField(instance.config.model);
-    fields = Object.keys(fields);
+    let keyFields = Object.keys(fields);
     let result= [];
-    records.forEach((item) => {
+    
+    records.forEach((instance) => {
         let a = [];
-        a.push(item[identifierField]); // for Actions
-        fields.forEach((key) => {
-            a.push(item[key]);
+        a.push(instance[identifierField]); // Push ID for Actions
+        keyFields.forEach((key) => {
+            if(fields[key].model.model){
+                // If not present displayField try get "name" field or identifierField
+                let displayField = fields[key].config.displayField;
+                if (!instance[key]) return a.push("")
+                // Relation
+                a.push(instance[key][displayField]);
+                
+            } else if (fields[key].model.collection) {
+                if (!instance[key] || !instance[key].length) return a.push("")
+                let displayField = fields[key].config.displayField;
+                
+                // Collections
+                let plainCollection = "";
+                instance[key].forEach((item)=>{
+                    if (plainCollection !== "") plainCollection += ", "
+                    plainCollection += !item[displayField] ? item[fields[key].config.identifierField] : item[displayField];
+                })
+                a.push(plainCollection);
+
+            } else {
+                a.push(instance[key]);
+            }
         });
         result.push(a);
     });
+
     res.json({
-        data: result,
-        //fileds: fields,
+        data: result
     });
 };
+
+
+async function waterlineExec(query) {
+    return new Promise((resolve, reject) => {
+        query.exec(function(err, records) {
+            if (err) reject(err);
+            resolve(records)
+        });
+      });
+}
