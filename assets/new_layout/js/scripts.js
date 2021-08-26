6f29dc7d71958ec6ef4d75c23b0286f220fd1bc2
Siziy Ivan <cziy@ya.ru>
2021-08-26 21:41:54 +0300

    add ejs

---
+++ assets/new_layout/js/scripts.js
@@ -0,0 +1,10 @@
+$(function(){
+
+   /* burger menu */
+   $('.header__menu').click(function(){
+      $(this).toggleClass('header__menu--active');
+      $('.sidenav-panel').toggleClass('sidenav-panel--active')
+   })
+   /* --- burger menu end --- */
+
+});
