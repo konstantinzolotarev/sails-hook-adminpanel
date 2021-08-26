8997329dc0b45c9ed36d47fd4c4f0826a2da2de5
Siziy Ivan <cziy@ya.ru>
2021-08-26 15:03:55 +0300

    bindAssets as middleware

--- lib/bindAssets.ts
+++ lib/bindAssets.ts
@@ -1,4 +1,7 @@
-
-export default async function(sails) {
-    console.log(">>>");
+import * as serveStatic from 'serve-static'
+import * as path from "path"
+export default async function(sails: any) {
+    sails.hooks.http.app.use('/admin/assets', serveStatic(path.join(__dirname, '../assets')));
+    let layer = sails.hooks.http.app._router.stack.slice(-1)[0] 
+    sails.hooks.http.app._router.stack.splice(1, 0, layer)
 };
