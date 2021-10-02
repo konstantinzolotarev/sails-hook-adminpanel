ff01ac91355f3e45ff62a6e9a56d8d18fdeeaa58
Siziy Ivan <cziy@ya.ru>
2021-10-02 05:38:50 +0300

    no move bindAssets

--- lib/bindAssets.ts
+++ lib/bindAssets.ts
@@ -2,6 +2,6 @@ import * as serveStatic from 'serve-static'
 import * as path from "path"
 export default async function(sails: any) {
     sails.hooks.http.app.use('/admin/assets', serveStatic(path.join(__dirname, '../assets')));
-    let layer = sails.hooks.http.app._router.stack.slice(-1)[0] 
-    sails.hooks.http.app._router.stack.splice(1, 0, layer)
+    //let layer = sails.hooks.http.app._router.stack.slice(-1)[0] 
+    //sails.hooks.http.app._router.stack.splice(1, 0, layer)
 };
